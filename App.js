96136ec48a6f11903303fa036cce50f26fc783c7
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-08-05 14:32:06 -0300

    instala e usa a fonte Inter com a variante Inter_900Black

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Inter_900Black, useFonts } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    Inter_900Black,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily: 'Inter_900Black'}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
